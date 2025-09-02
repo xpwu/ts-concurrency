71fdb72893735c42bb8beeef7d0fef9b53f9037f
xpwu <diodexp@foxmail.com>
2025-09-02 22:21:43 +0800

    add: timeout --- canceled()

--- src/timeout.ts
+++ src/timeout.ts
@@ -9,15 +9,17 @@ export class Timeout implements Error {
 	}
 }
 
-export async function withTimeout<R>(d: Duration, exe: ()=>Promise<R>): Promise<R|Timeout> {
+export async function withTimeout<R>(d: Duration, exe: (canceled: ()=>boolean)=>Promise<R>): Promise<R|Timeout> {
 	let timer
+	let canceled = false
 	let timePro = new Promise<Timeout>((resolve)=>{
 		timer = setTimeout(()=>{
+			canceled = true
 			resolve(new Timeout(d))
 		}, d/Millisecond)
 	})
 
-	let ret = await Promise.race([exe(), timePro])
+	let ret = await Promise.race([exe(()=>canceled), timePro])
 	clearTimeout(timer)
 	return ret
 }
